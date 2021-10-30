143272ea1df23ce4dfec9b4f3105c5a6649f5735
TranHuuPhuc <tranhuuphuc.tdc2019@gmail.com>
2021-10-30 14:57:58 +0700

    update project

--- form_edit_product.php
+++ form_edit_product.php
@@ -24,67 +24,60 @@ $selected = "";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Form</title>
-    <?php include 'views/meta.php';?>
+    <?php include 'views/meta.php'; ?>
     <script src="./public/js/product_script.js"></script>
 </head>
 
 <body>
     <div class="container">
-        <?php if ($product || !isset($_id)) {?>
-        <form action="editProduct.php" method="post" enctype="multipart/form-data">
-            <input type="hidden" name="id" value="<?= $_id; ?>" />
-            <div class="form-group">
-                <label class="control-label">Choose an image :</label>
-                <div class="controls">
-                    <img width="100" height="100"
-                        src="./public/images/<?= $product['product_photo']?>"
-                        alt="">
-                    <p><?= $product['product_photo']?>
-                    </p>
-                    <input type="file" name="fileUpload" id="fileUpload">
+        <?php if ($product || !isset($_id)) { ?>
+            <form action="editProduct.php" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="id" value="<?= $_id; ?>" />
+                <div class="form-group">
+                    <label class="control-label">Choose an image :</label>
+                    <div class="controls">
+                        <img width="100" height="100" src="./public/images/<?= $product['product_photo'] ?>" alt="">
+                        <p><?= $product['product_photo'] ?>
+                        </p>
+                        <input type="file" name="fileUpload" id="fileUpload">
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="name">Name</label>
-                <input class="form-control" name="name" placeholder="Name"
-                    value="<?= $product['product_name']?>">
-            </div>
-            <div class="form-group">
-                <label for="categories">Categories</label>
-                <select class="form-control" name="cate">
-                    <?php foreach ($categories as $cate) {
-    if ($product['id'] == $cate['product_id']) {
-        $selected = "selected"; ?>
-                    <option
-                        value="<?php echo $cate['category_id']?>"
-                        <?php echo $selected ?>><?php echo $cate['category_name']?>
-                    </option>
-                    <?php } else {?>
-                    <option
-                        value="<?php echo $cate['category_id']?>">
-                        <?php echo $cate['category_name']?>
-                        <?php } } ?>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="description">Description</label>
-                <div class="controls">
-                    <textarea class="span11"  cols="70" rows="10"
-                        placeholder="Description" name="description"><?= $product['product_description']?></textarea>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input class="form-control" name="name" placeholder="Name" value="<?= $product['product_name'] ?>">
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="price">Price</label>
-                <input type="price" name="price" class="form-control" placeholder="price"
-                    value="<?= $product['product_price']?>">
-            </div>
-            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-        </form>
+                <div class="form-group">
+                    <label for="categories">Categories</label>
+                    <select class="form-control" name="cate">
+                        <?php foreach ($categories as $cate) {
+                            if ($product['id'] == $cate['product_id']) {
+                                $selected = "selected"; ?>
+                                <option value="<?php echo $cate['category_id'] ?>" <?php echo $selected ?>><?php echo $cate['category_name'] ?>
+                                </option>
+                            <?php } else { ?>
+                                <option value="<?php echo $cate['category_id'] ?>">
+                                    <?php echo $cate['category_name'] ?>
+                            <?php }
+                        } ?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <div class="controls">
+                        <textarea class="span11" cols="70" rows="10" placeholder="Description" name="description"><?= $product['product_description'] ?></textarea>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="price">Price</label>
+                    <input type="price" name="price" class="form-control" placeholder="price" value="<?= $product['product_price'] ?>">
+                </div>
+                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+            </form>
         <?php } else { ?>
-        <div class="alert alert-success" role="alert">
-            Product not found!
-        </div>
-        <?php }?>
+            <div class="alert alert-success" role="alert">
+                Product not found!
+            </div>
+        <?php } ?>
     </div>
 </body>
 
