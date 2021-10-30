c8236d2053fd6a8dec06b1330ddab60087cbafa6
TranHuuPhuc <tranhuuphuc.tdc2019@gmail.com>
2021-10-30 16:21:21 +0700

    update TrangChiTiet-ChiTietSanPham

--- product.php
+++ product.php
@@ -43,6 +43,7 @@ $item = $productModel->getProductById($id);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
+    <!-- Hiển thị sản phẩm trang chi tiết sản phẩm -->
     <div class="container">
         <div class="row">
             <div class="col-md-4">
