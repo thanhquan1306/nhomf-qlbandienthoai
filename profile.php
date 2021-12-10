47b233d230be18d1a9892106270aefbd845ea163
TranHuuPhuc <tranhuuphuc.tdc2019@gmail.com>
2021-12-10 22:29:36 +0700

    Revert "Merge branch 'thinh-userProfile' into master-web2"
    This reverts commit 232afeff9f6bdce4d7cc8bc03b7352224cee6cd1, reversing
    changes made to 9e84bbf2e0a327cdc6f613d6e6aa5465aa3c6fc9.

--- profile.php
+++ profile.php
@@ -11,6 +11,7 @@ $userModel = new UserModel();
 
 $user = null;
 $_id = null;
+
 if (!empty($_GET['id'])) {
     $_id = $_GET['id'];
     $user = $userModel->findUserById($_id);
@@ -19,48 +20,14 @@ if (!empty($_POST['submit'])) {
     $id = $_POST['id'];
     $name = $_POST['name'];
     $email = $_POST['mail'];
-    // $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $password = $user['password'];
+    $password = $_POST['password'];
 
-    if ($name != $user['name'] || $email != $user['email']) {
-        $userModel->updateUsers($name, $email, $password, $id);
-        echo '<script>alert("Cập nhật thành công!");window.location.href="../profile.php?id='.$user['id'].'"</script>';
+    $userModel->updateUsers($name, $email, $password, $id);
+    if (!($email == $user['email'])) {
+        header('location: profile.php?id='.$id);
     }
-    else {
-        echo '<script>alert("Không thành công!");</script>';
-    }
-    // $userModel->updateUsers($name, $email, $password, $id);
-    // // if (!($email == $user['email']) || !($name == $user['name'])) {
-    //     echo '<script>alert("Cập nhật thành công!");</script>';
-    //     header('location: profile.php?id=' . $id);
-    // // }
 }
-if (isset($_POST['doiMatKhau'])) {//if (!empty($_POST['doiMatKhau'])) {
-    $id = $_POST['id'];
-    $name = $_POST['name'];
-    $email = $_POST['mail'];
-    $passold = password_verify($_POST['passwordOld'], $user['password']);
-    $passnew1 = $_POST['passwordNew1'];//password_hash($_POST['passwordNew1'], PASSWORD_DEFAULT);
-    $passnew2 = $_POST['passwordNew2'];//password_hash($_POST['passwordNew2'], PASSWORD_DEFAULT);
-    
-    if ($passold == true) {
-        if ($passnew1 == $passnew2) {
-            $pass = password_hash($passnew1, PASSWORD_DEFAULT);
-            $userModel->updateUsers($name, $email, $pass, $id);
-            echo "<script>alert('Đổi mật khẩu thành công!');window.location.href='../profile.php?id=".$user['id']."'</script>";
-        }
-        else {
-            echo "<script>alert('Xác nhận mật khẩu sai!');</script>";
-        }
-    }
-    else {
-        echo "<script>alert('Sai mật khẩu!');</script>";
-    }
-    // echo "<script>alert('Cập nhật thành công!');</script>";
-    // if (password_verify($passold, $user["password"])) {
-    //     echo "<script>alert('Cập nhật thành công!');</script>";
-    // }
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -77,8 +44,8 @@ if (isset($_POST['doiMatKhau'])) {//if (!empty($_POST['doiMatKhau'])) {
 </head>
 
 <body>
-
-    <nav class="navbar navbar-expand-sm ">
+    
+<nav class="navbar navbar-expand-sm ">
         <a class="navbar-brand" href="./index.php"><img src="./public/images/smartphone (1).png" alt=""></a>
         <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -86,81 +53,46 @@ if (isset($_POST['doiMatKhau'])) {//if (!empty($_POST['doiMatKhau'])) {
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0 has">
                 <li class="nav-item active">
-                    <h1 style="margin-top: 18px; color: #fff;">Hồ sơ </h1>
+                    <h1>Giỏ hàng</h1>
                 </li>
             </ul>
         </div>
     </nav>
-
-    <div class="content row">
-
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
-            <?php if ($user || !isset($_id)) { ?>
-                <div class="content-text">
-                    <h4>Hồ sơ của tôi</h4>
-                    <p>Quản lý thông tin hồ sơ để bảo mật tài khoản</p>
-                </div>
-                <hr>
-                <div class="content-profile">
-                    <form method="post">
-                        <input type="hidden" name="id" value="<?php echo $_id; ?>">
-                        <div class="form-group row">
-                            <label class="col-md-3" for="username">Tên đăng nhập: </label>
-                            <input class="form-control col-md-3" name="username" placeholder="UserName" value='<?php if (!empty($user['username'])) echo $user['username']; ?>' readonly>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-3" for="name">Họ tên: </label>
-                            <input class="form-control col-md-3" name="name" placeholder="Name" value="<?php if (!empty($user['name'])) echo $user['name']; ?>">
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-3" for="email">Email: </label>
-                            <input class="form-control col-md-3" name="mail" placeholder="Email" value="<?php if (!empty($user['email'])) echo $user['email']; ?>">
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-md-3" for="password">Password: </label>
-                            <input type="password" class="form-control col-md-3" name="password" placeholder="Password" value="<?php if (!empty($user['password'])) echo $user['password']; ?>"readonly>
-                            <div class="box">
-                                <a href="#popup1" class="button">Đổi mật khẩu</a>
-                            </div>
-                            <div id="popup1" class="overlay">
-                                <div class="popup">
-                                    <h3>Đổi mật khẩu</h3>
-                                    <a href="#" class="close">&times;</a>
-                                    <div class="ct-txt">
-                                        <!-- <form action="#"> -->
-                                        <div class="form-group">
-                                            <label class="passOld" for="passwordOld">Password cũ: </label>
-                                            <input type="password" name="passwordOld" placeholder="Password cũ">
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="passNew" for="">Password mới: </label>
-                                            <input type="password" name="passwordNew1" placeholder="Password mới">
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="passNew2" for="">Xác nhận password: </label>
-                                            <input type="password" name="passwordNew2" placeholder="Xác nhận password">
-                                        </div>
-                                        <input type="submit" name="doiMatKhau" value="Thay đổi" class="btn btn-primary change" >
-                                        <!-- </form> -->
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" name="submit" value="submit" class="btn btn-primary save" onclick="return confirm('Bạn xác nhận muốn cập nhật thông tin?')">Lưu</button>
-                        <input type="button" value="Thoát" class="btn btn-primary logout" onclick="document.location='index.php'">
-                    </form>
-                </div>
-            <?php } else { ?>
-                <div class="alert">
-                    <script>
-                        alert('User not found!');
-                    </script>
-                </div>
-            <?php } ?>
+    <?php if ($user || !isset($_id)) { ?>
+        <div class="content-text">
+            <h4>Hồ sơ của tôi</h4>
+            <p>Quản lý thông tin hồ sơ để bảo mật tài khoản</p>
+            <hr>
         </div>
-        <div class="col-md-3"></div>
-    </div>
+        <div class="content-profile">
+
+            <form action="" method="post">
+                <input type="hidden" name="id" value="<?php echo $_id; ?>">
+                <div class="form-group row">
+                    <label class="col-md-2" for="username">Tên đăng nhập: </label>
+                    <input class="form-control col-md-3" name="username" placeholder="UserName" value='<?php if (!empty($user['username'])) echo $user['username']; ?>'>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2" for="name">Họ tên: </label>
+                    <input class="form-control col-md-3" name="name" placeholder="Name" value="<?php if (!empty($user['name'])) echo $user['name']; ?>">
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2" for="email">Email: </label>
+                    <input class="form-control col-md-3" name="mail" placeholder="Email" value="<?php if (!empty($user['email'])) echo $user['email']; ?>">
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-2" for="password">Password: </label>
+                    <input type="password" class="form-control col-md-3" name="password" placeholder="Password" value="<?php if (!empty($user['password'])) echo $user['password']; ?>">
+                    <a class="col-md-1" href="">Thay đổi</a>
+                </div>
+                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    <?php } else { ?>
+        <div class="alert">
+            <script> alert('User not found!');  </script>
+        </div>
+    <?php } ?>
 </body>
 
 </html>
